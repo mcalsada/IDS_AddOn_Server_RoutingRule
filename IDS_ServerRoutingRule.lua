6de41bc76dab50bccedc922a0cad5143a388ba1a
Michael C. Mulligan <mulligam@upstate.edu>
2013-03-12 08:31:01 -0400

    Initial commit of the code.

---
+++ IDS_ServerRoutingRule.lua
@@ -0,0 +1,85 @@
+-- IDS_ServerRoutingRule.lua
+--
+-- Bill Jones
+-- Michael C. Mulligan
+--
+
+local Settings = {};
+
+Settings.NVTGC = GetSetting("NVTGC");
+Settings.ProcessType = GetSetting("ProcessType");
+Settings.TranasctionStatus = GetSetting("TransactionStatus");
+Settings.MatchString = GetSetting("MatchString");
+Settings.NewProcessType = GetSetting("NewProcessType");
+Settings.NewTransactionStatus = GetSetting("NewTransactionStatus");
+
+Settings.OCLCSymbol = '';
+
+function Init()
+	RegisterSystemEventHandler("SystemTimerElapsed", "TimerElapsed");
+end
+
+function TimerElapsed(eventArgs)
+	setOCLCSymbol()
+	local tns = getTransactionsToProcess();
+	ProcessDataContexts("TransactionNumber", tns, "ProcessTransaction");
+end
+
+function setOCLCSymbol()
+	local q = "select OCLCSymbol from LocalInfo where NVTGC='" .. Settings.NVTGC .. "'";
+	Settings.OCLCSymbol = getScalar(q);
+end
+
+function ProcessTransaction()
+	local currentTN = GetFieldValue("Transaction", "TransactionNumber");
+	SetFieldValue("Transaction", "ProcessType", Settings.NewProcessType);
+	ExecuteCommand("Route", {currentTN, Settings.NewTransactionStatus});
+	SaveDataSource("Transaction");
+end
+
+function getTransactionsToProcess()
+	local usersTable = getUsersTable();
+	local q = '';
+	q = q .. "select t.TransactionNumber ";
+	q = q .. "from Transactions t, " .. usersTable .. " u ";
+	q = q .. "where ( t.Username = u.Username ) ";
+	q = q .. "and ( u.NVTGC = '" .. Settings.NVTGC .. "' ) ";
+	q = q .. "and ( t.ProcessType = '" .. Settings.ProcessType .. "' ) ";
+	q = q .. "and ( t.TransactionStatus = '" .. Settings.TransactionStatus .. "' ) ";
+	q = q .. 'and ( ' .. Settings.MatchString .. ' ) ';
+	return getTransactions(q);
+end
+
+function getTransactions(query)
+	local connection = CreateManagedDatabaseConnection();
+	connection.QueryString = query;
+	connection:Connect();
+	local resultsTable = connection:Execute();
+	local transactionNumbers = {};
+	for i = 0, resultsTable.Rows.Count - 1 do
+		transactionNumbers[i] = resultsTable.Rows:get_Item(i):get_Item("TransactionNumber");
+	end
+	connection:Disconnect();
+	connection:Dispose();
+	return transactionNumbers
+end
+
+function getScalar(query)
+	local value = '';
+	local connection = CreateManagedDatabaseConnection();
+	connection.QueryString = query;
+	connection:Connect();
+	value = connection:ExecuteScalar();
+	connection:Disconnect();
+	connection:Dispose();
+	return value;
+end
+
+function getUsersTable()
+	local usersTable = 'Users';
+	local shared = getScalar("select Value from Customization where CustKey = 'SharedServerSupport' and NVTGC = 'ILL'");
+	if (shared == "Yes") then
+		usersTable = 'UsersAll';
+	end
+	return usersTable;
+end
